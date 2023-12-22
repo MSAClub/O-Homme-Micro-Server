01f93a3d78ea21047e681ac1cbb61187c047cdba
ChoHyunWoo <wizardchw0@gmail.com>
2023-12-22 17:17:52 +0900

    proj. settings

--- review/src/main/java/com/msaclub/review/ReviewApplication.java
+++ review/src/main/java/com/msaclub/review/ReviewApplication.java
@@ -2,12 +2,18 @@ package com.msaclub.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReviewApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("hi");
 		SpringApplication.run(ReviewApplication.class, args);
+		System.out.println("bye");
+
 	}
 
 }
